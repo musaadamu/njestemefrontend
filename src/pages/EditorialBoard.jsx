2ca9569a46531ef76db818710ac74a8fba2274cc
Musa Adamu <msmaje2024@gmail.com>
2026-07-05 05:54:14 +0100

    aminuoriginalnumber

--- src/pages/EditorialBoard.jsx
+++ src/pages/EditorialBoard.jsx
@@ -39,7 +39,7 @@ export default function EditorialBoard() {
 
   const editorialSecretary = {
     name: "Musah Aminu, PhD",
-    Phone: "+234 813 861 4901"
+    Phone: "+234 803 614 1266"
   };
 
   return (
